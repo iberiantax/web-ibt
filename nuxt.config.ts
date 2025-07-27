3df6f53cf858926aac3e99c663e330c421de3a32
Caterina Aracil <caterinaaracil@gmail.com>
2025-07-27 22:14:12 +0200

    new logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: true,
-    compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
-    modules: [
-        '@nuxtjs/i18n',
-        '@nuxtjs/sitemap',
-        '@nuxtjs/robots',
-        '@nuxtjs/tailwindcss'
+  ssr: true,
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/i18n', '@nuxtjs/sitemap', '@nuxtjs/robots', '@nuxtjs/tailwindcss'],
+  css: ['@/assets/css/main.css'],
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+    },
+  },
+  site: {
+    url: 'https://www.iberiantax.com',
+    name: 'IberianTax',
+  },
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US',
+        name: 'English',
+        file: 'en.json',
+      },
+      {
+        code: 'es',
+        iso: 'es-ES',
+        name: 'Español',
+        file: 'es.json',
+      },
     ],
-    css: ['@/assets/css/main.css'],
-    site: {
-        url: 'https://www.iberiantax.com',
-        name: 'IberianTax'
+    strategy: 'prefix_except_default',
+    defaultLocale: 'en',
+    customRoutes: 'config',
+    pages: {
+      about: {
+        en: '/about-us',
+        es: '/sobre-nosotros',
+      },
+      blog: {
+        en: '/blog',
+        es: '/blog',
+      },
     },
-    i18n: {
-        locales: [
-            {
-                code: 'en',
-                iso: 'en-US',
-                name: 'English',
-                file: 'en.json',
-            },
-            {
-                code: 'es',
-                iso: 'es-ES',
-                name: 'Español',
-                file: 'es.json',
-            },
-        ],
-        strategy: 'prefix_except_default',
-        defaultLocale: 'en',
-        customRoutes: 'config',
-        pages: {
-            about: {
-                en: '/about-us',
-                es: '/sobre-nosotros',
-            },
-            blog: {
-                en: '/blog',
-                es: '/blog',
-            }
-        }
-    },
-})
+  },
+});
